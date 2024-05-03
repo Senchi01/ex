4423f3c24ee73d1f52ea483bf66174d3c9c8894b
Ghiath Alomari <ga222kt@student.lnu.se>
2024-05-03 14:03:47 +0200

    "sunday is bugged"

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRaisedButton
@@ -42,42 +43,101 @@ class MainApp(MDApp):
 
 
     def parking_rule(self, wordList):
-       current_date = datetime.datetime.now()
-       dayName = current_date.strftime('%A')
-       currentHour = current_date.hour
+      current_date = datetime.datetime.now()
+      dayName = current_date.strftime('%A')
+      currentHour = current_date.hour
 
-       if len(wordList) == 1 and wordList[0] == 'P':
-          return "Parking is permitted for 24 hours"
-       elif wordList[0] == 'P' and wordList[1] == '&':
-          return "Parking is permitted for Handicap only"
-       elif 'Avgift' in wordList:
-          avgiftIndex = wordList.index('Avgift')
-          if 'tim' in wordList[avgiftIndex+1]: 
-            elmt = wordList[avgiftIndex+1]
-            time = elmt[0]
-            timeIndex = wordList.index(elmt)
-            return self.handleFee(dayName,wordList,timeIndex,currentHour,time)   
+      if len(wordList) == 1 and wordList[0] == 'P':
+        return "Parking is permitted for 24 hours"
+      elif wordList[0] == 'P' and wordList[1] == '&':
+        return "Parking is permitted for Handicap only"
+      elif 'Avgift' in wordList:
+        avgiftIndex = wordList.index('Avgift')
+        if 'tim' in wordList[avgiftIndex+1]: 
+          elmt = wordList[avgiftIndex+1]
+          time = elmt[0]
+          timeIndex = wordList.index(elmt)
+          if dayName not in ['Saturday', 'Sunday']:
+            return self.handleFees(wordList,timeIndex,currentHour,time,isFee=True)
+          elif dayName == 'Saturday':
+              for i in wordList:
+                 if '(' in i:
+                    elmtIndex = wordList.index(i)
+                    return self.handleFees(wordList,elmtIndex-1,currentHour,time,isFee=True)  
+                 else:
+                    return f"parking is free"  
+          else:
+            for i in wordList:
+              if '(' in i:
+                elmtIndex = wordList.index(i)
+                return self.handleFees(wordList,elmtIndex-1,currentHour,time,isFee=True)
+              else:
+                    return f"parking is free"  
+        elif 'tim' in wordList[avgiftIndex-1]:
+          elmt = wordList[avgiftIndex-1]
+          time = elmt[0]
+          timeIndex = wordList.index(elmt)
+          if dayName not in ['Saturday', 'Sunday']:
+            return self.handleFees(wordList,avgiftIndex,currentHour,time,isFee=True)
+          elif dayName == 'Saturday':
+              for i in wordList:
+                 if '(' in i:
+                    elmtIndex = wordList.index(i)
+                    return self.handleFees(wordList,elmtIndex-1,currentHour,time,isFee=True) 
+                 else:
+                    return f"parking is free"    
+          else:
+            for i in wordList:
+              if '(' in i:
+                elmtIndex = wordList.index(i)
+                return self.handleFees(wordList,elmtIndex-1,currentHour,time,isFee=True)
+              else:
+                    return f"parking is free"  
+      elif 'P-tillstånd' in wordList:
+        return f'Parking not permitted. permission needed!'
+      elif 'Avgift' not in wordList:
+        for elmt in wordList:
+           if 'tim' in elmt:
+              time = elmt[0]
+              timeIndex = wordList.index(elmt)
+              return self.handleFees(wordList,timeIndex,currentHour,time,isFee=False)
 
-          elif 'tim' in wordList[avgiftIndex-1]:
-            elmt = wordList[avgiftIndex-1]
-            time = elmt[0]
-            return self.handleFee(dayName,wordList,avgiftIndex,currentHour,time)   
 
-    def handleFee(self, dayName,wordList,index, ct, time ):
-       if dayName not in ['Saturday', 'Sunday']:
-              timeInterval = wordList[index+1]
-              timeSplitted = timeInterval.split('-')
-              startTime = int(timeSplitted[0])
-              endTime = int(timeSplitted[1])
-              if startTime < ct and ct < endTime:
-                if (ct + int(time)) <= endTime:
-                  return "Parking is permitted for max " + time + " hours with fee"
-                elif (ct + int(time)) > endTime:
-                  newTime = (ct + int(time)) - endTime
-                  if newTime < time :
-                    return "Parking is permitted for " + time + " hours with fee then it's free"
-                else:
-                    return "Parking is permitted to %d", startTime
+       
+
+
+    def handleFees(self,wordList,index, ct, time, isFee ):
+        if len(wordList) > index + 1:
+           index += 1
+        else:
+           index = index
+        timeInterval = wordList[index]
+        timeIntervalIndex = wordList.index(timeInterval)
+        timeInterval = re.sub(r'[^\d-]', '', timeInterval) 
+        timeSplitted = timeInterval.split('-')
+        startTime = int(timeSplitted[0])
+        if startTime in ['B',88, '&']:
+            startTime = 8
+        endTime = int(timeSplitted[1])
+         # Convert `time` by extracting digits and turning into an integer
+        timeDigits = re.findall(r'\d+', time)  # Finds all digit groups
+        if timeDigits:
+          time = int(timeDigits[0])  # Take the first group of digits found
+        if startTime < ct and ct < endTime:
+          if (ct + int(time)) <= endTime and isFee:
+            return f"Parking is permitted for max {time} hours with fee"
+          elif (ct + int(time)) > endTime:
+            newTime = (ct + int(time)) - endTime
+            if newTime < int(time) and isFee :
+              return f"Parking is permitted for {newTime} hours with fee then it's free"
+            else:
+              return f"Parking is permitted for free"
+          else:
+            return f"Parking is permitted for free"
+        elif len(wordList) > timeIntervalIndex+1 and ct < startTime:
+          return f"Parking is free to {startTime}"
+        else:
+          return f"Parking is permitted"
                 
 
     def take_picture(self, *args):
